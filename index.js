f57e206d5de13ee0c53d96c368ad8a5bc09e1920
Larry Otieno <larryotieno51@gmail.com>
2025-02-26 15:36:11 +0300

    Adds loops

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Larry", "Paul", "Carol"];
+
+function writeCards (names, wedding) {
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${wedding} gift!`);
+    }
+
+    return messages;
+}
+
+function countDown(number) {
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+   
+}
+countDown(10);
